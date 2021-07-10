2780752d27db233454368286b7738de31343d1a2
SurajGupta07 <bgs071998@gmail.com>
2021-07-10 21:14:56 +0530

    Styles: Posts css

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -2,10 +2,31 @@ import Video from "../assets/banner.mp4"
 
 const Posts = () => {
     return (
-        <div className="posts__container">
-            <video style={{borderRadius: '10px'}} width="835" height="623" autoPlay muted>
-                <source src={Video} type="video/mp4"/>
-            </video>
+        <div>
+            <div className="posts__container">
+                <video
+                    style={{
+                    borderRadius: '10px'
+                }}
+                    width="835"
+                    height="623"
+                    autoPlay
+                    muted>
+                    <source src={Video} type="video/mp4"/>
+                </video>
+            </div>
+            <div className="short-description__container">
+                <p
+                    style={{
+                    fontFamily: "'Haas Grot Text R Web', 'Helvetica Neue', Helvetica, Arial, sans-serif",
+                    fontSize: "20px",
+                    fontWeight: "400",
+                    lineHeight: "32px"
+                }}>Hi there,
+                    <br/>
+                    I'm starting a new project (just for fun as usual). The brand is Bower &
+                    Wilkins. In this shot you can find a carousel with smooth transitions.</p>
+            </div>
         </div>
     )
 }
