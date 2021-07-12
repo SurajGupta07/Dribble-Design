722a1bb009316ad6f29998303ce6f19e6fa113f7
SurajGupta07 <bgs071998@gmail.com>
2021-07-12 19:48:11 +0530

    Refactor: Footer bottom

--- src/components/Footer/Lower.jsx
+++ src/components/Footer/Lower.jsx
@@ -1,6 +1,8 @@
+import DribbleIconFooter from "../../assets/dribbleFooter.svg"
+
 const Lower = () => {
     return (
-        <div>
+        <div className="footer__border">
             <hr className="user-avatar__border" />
             <div className="footer-lower">
                 <div>© 2021 Dribbble. All rights reserved.</div>
@@ -9,6 +11,7 @@ const Lower = () => {
                         <span class="shots-count">16,018,703{" "}
                         </span>
                         <span className="shots-text">shots dribbbled</span>
+                        <img className="dribble-ball" src={DribbleIconFooter} alt="Dribble Icon"/>
                     </div>
                 </div>
             </div>
